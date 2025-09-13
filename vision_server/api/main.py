a8a6c8b0813b2b38f32b8a713b6929fc0b84b677
astarryknight <whiteh4tter@gmail.com>
2025-09-13 14:27:51 -0400

    vision_server - add CORS support and fix python dependency issues

--- vision_server/api/main.py
+++ vision_server/api/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request, Response
+from flask_cors import CORS
 import os
 import numpy as np
 import face_recognition
@@ -8,10 +9,37 @@ import io
 import cv2
 
 app = Flask(__name__)
+CORS(
+ app,
+ resources={
+  r"/*": {
+   "origins": "*",
+   "methods": [
+    "GET",
+    "POST",
+    "PUT",
+    "PATCH",
+    "DELETE",
+    "OPTIONS",
+   ],
+   "allow_headers": [
+    "Content-Type",
+    "Authorization",
+    "Access-Control-Allow-Origin"
+   ],
+  }
+ },
+)
+
+
+def allowed_file(filename):
+    '''Check if the uploaded file has an allowed extension'''
+    ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'bmp', 'webp'}
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def np_from_img(file):
      '''converts a buffer from a tar file in np.array'''
-     nparr = np.frombuffer(file, np.uint8)
+     nparr = np.frombuffer(file.read(), np.uint8)
      img_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
      return img_np
 
